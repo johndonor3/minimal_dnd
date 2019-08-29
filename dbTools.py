26796735c429d913544c22e8ffd1a8dc05651055
johndonor3 <j.donor@tcu.edu>
2019-08-28 19:29:18 -0500

    it's sort of working!

---
+++ dbTools.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+import aiosqlite
+
+from quart import g
+from quart import current_app as app
+
+async def connect_db():
+    engine = await aiosqlite.connect(app.config['DATABASE'])
+    engine.row_factory = aiosqlite.Row
+    return engine
+
+
+async def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = await connect_db()
+    return g.sqlite_db
+
+
+async def getCharacters():
+    """grab everything
+    """
+    db = await get_db()
+    cur = await db.execute(
+    """SELECT *
+          FROM characters
+      ORDER BY id DESC""",
+    )
+    # posts = cur.fetchall()
+    return await cur.fetchall()
+
+
+async def getCharacter(name):
+    db = await get_db()
+    cur = await db.execute(
+    """SELECT *
+          FROM characters
+      WHERE name == '{}' """.format(name),
+    )
+    # posts = cur.fetchall()
+    return await cur.fetchone()
+
+
+async def addCharacter(name, base, abilities):
+    iniative = base.get("iniative", 0)
+    speed = base.get("speed", 30)
+    proficiency = base.get("proficiency", 2)
+    hp = base.get("hp", 10)
+    db = await get_db()
+    await db.execute(
+         """INSERT INTO characters (name, hp, iniative, speed, proficiency) 
+                VALUES (?, ?, ?, ?, ?)""",
+         [name, hp, iniative, speed, proficiency],
+    )
+    await db.commit()
+
+    newChar = await getCharacter(name)
+    character_id = newChar["id"]
+    strength = abilities.get("strength", 10)
+    dexterity = abilities.get("dexterity", 10)
+    constitution = abilities.get("constitution", 10)
+    intelligence = abilities.get("intelligence", 10)
+    wisdom = abilities.get("wisdom", 10)
+    charisma = abilities.get("charisma", 10)
+    await db.execute(
+         """INSERT INTO abilities (character_id, strength, dexterity, constitution, 
+                                   intelligence, wisdom, charisma) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)""",
+         [character_id, strength, dexterity, constitution, intelligence, wisdom, charisma],
+    )
+
+    
+
+
+async def updateCharacter(name, attr, value):
+    assert len(attr) == len(value), "mis-matched update!"
+    db = await get_db()
+    update = "SET"
+    for a, v in zip(attr, value):
+        update += " {a} = {v},".format(a=a, v=v)
+    update = update[:-1]  # strip the last comma
+    await db.execute(
+         "UPDATE characters {} WHERE name == '{}'".format(update, name),
+    )
+    await db.commit()
---
+++ schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS characters;
+CREATE TABLE characters (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name TEXT NOT NULL,
+   hp INT NOT NULL,
+   iniative INT NOT NULL,
+   speed INT NOT NULL,
+   proficiency INT NOT NULL
+);
+
+DROP TABLE IF EXISTS abilities;
+CREATE TABLE abilities (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   character_id INT NOT NULL,
+   strength INT NOT NULL,
+   dexterity INT NOT NULL,
+   constitution INT NOT NULL,
+   intelligence INT NOT NULL,
+   wisdom INT NOT NULL,
+   charisma INT NOT NULL
+);
