724152ea87da257bb6176d2bfdca0fa14b7328ac
johndonor3 <j.donor@tcu.edu>
2019-09-12 21:25:20 -0500

    react is working for a 'table'

--- static/js/table.js
+++ static/js/table.js
@@ -31,40 +31,38 @@ class AbilTable extends React.Component {
   }
 }
 
-class Purse extends React.Component {
 
-  createTable = () => {
-    let table = []
-    let children = []
-    children.push(<th width="40"></th>)
-    children.push(<th width="20"></th>)
+class Coin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {nom: props.nom, val: props.val};
+  }
 
-    table.push(<tr>{children}</tr>)
-
-    for (let i = 0; i < j_purse.length; i++) {
-        let children = []
-        //Inner loop to create children
-        children.push(<td> <b>{j_purse[i].coin}</b>  </td>)
-        children.push(<td> {j_purse[i].val}  </td>)
-        //Create the parent and add the children
-        table.push(<tr>{children}</tr>)
-    }
-    return table
+  test(){
+    var newCoin = prompt("Enter new value for "+this.state.nom, this.state.val);
+    console.log(newCoin, this.state.nom, this.state.val);
   }
 
   render() {
-    return(
-      <table>
-      <tbody>
-        {this.createTable()}
-      </tbody>
-      </table>
-    )
+    return (
+      <p>
+        <b>{this.state.nom}</b>: {this.state.val}
+        <button onClick={this.test.bind(this)}>$</button>
+      </p>
+
+    );
   }
 }
 
-const Element = Purse
-ReactDOM.render(<Element />, document.getElementById('purse_table'));
+const purse = (
+  <div>
+  {j_purse.map(item => (<Coin key={item.coin} nom={item.coin} val={item.val}/>)
+    )}
+  </div>
+)
+
 
 const Table = AbilTable
 ReactDOM.render(<Table />, document.getElementById('abil_table'));
+
+ReactDOM.render(purse, document.getElementById('purse_table'));
