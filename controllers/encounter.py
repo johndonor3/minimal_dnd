06a4a9ba2210b2556968c5c903737151aba95479
John Donor <j.donor@tcu.edu>
2020-06-27 17:50:04 -0500

    encounter character init works

--- controllers/encounter.py
+++ controllers/encounter.py
@@ -17,7 +17,6 @@ async def encounter():
     form = await request.form
 
     if "hp" in form:
-        print(form)
         delta = int(form["hp"])
         name = form["name"]
 
---
+++ static/js/encounter.js
@@ -0,0 +1,72 @@
+function updateHP(name, delta){
+
+    const form = document.createElement('form');
+    form.method = "post";
+    form.action = "/encounter.html";
+
+
+    const hiddenField = document.createElement('input');
+    hiddenField.type = 'hidden';
+    hiddenField.name = "hp";
+    hiddenField.value = delta;
+    form.appendChild(hiddenField);
+
+    const hiddenField2 = document.createElement('input');
+    hiddenField2.type = 'hidden';
+    hiddenField2.name = "name";
+    hiddenField2.value = name;
+    form.appendChild(hiddenField2);
+    
+    document.body.appendChild(form);
+    form.submit();
+}
+
+
+class EncChar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: props.name, hp: props.hp, 
+                  init: window.localStorage.getItem(props.name+'init') || ''};
+  }
+
+  componentDidUpdate(){
+    window.localStorage.setItem(this.state.name+'init', this.state.init);
+  }
+
+  updateInit(event){
+    console.log(event.target.value)
+    let parsed = parseInt(event.target.value);
+    if (isNaN(parsed)) {
+      this.setState({init: event.target.value});
+    }
+    else {
+      this.setState({init: parsed});
+    }
+    
+  }
+
+  render() {
+    return (
+      <div className="char-row">
+          <p><span className="charName">{this.state.name}  </span>
+          <button onClick={() => updateHP(this.state.name, -1)}>-</button>
+          <strong> {this.state.hp} </strong>
+          <button onClick={() => updateHP(this.state.name, 1)}>+</button>
+          <span>   </span>
+          <input value={this.state.init} onChange={this.updateInit.bind(this)} />
+          </p>
+      </div>
+    );
+  }
+}
+
+
+let charInfo = (
+  <div className="char-encounter-table">
+      {enc_chars.map( c => (
+          <EncChar key={c.name} name={c.name} hp={c.hp}/>
+      ))}
+  </div>
+)
+
+ReactDOM.render(charInfo, document.getElementById('encounter-characters'));
